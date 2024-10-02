7f7a5663d2659ab1f971d658ac25c585d38ce9af
Gian Singh <73843250+Giansingh4710@users.noreply.github.com>
2024-10-02 15:59:32 -0400

    to get rid of random errors from elements

---
+++ src/global.d.ts
@@ -0,0 +1,15 @@
+declare namespace JSX {
+  interface IntrinsicElements {
+    div: any; // Add any specific intrinsic elements as needed
+    span: any;
+    nav: any;
+    ul: any;
+    section: any;
+    h1: any;
+    p: any;
+    button: any;
+    img: any;
+    li: any;
+    // Add custom elements if you have any
+  }
+}
